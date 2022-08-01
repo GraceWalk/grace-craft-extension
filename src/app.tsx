efd000e327d7c52ac5485323ccdd84fb15a09344
GraceWalk <gracewalk1999@163.com>
2022-08-01 13:39:37 +0800

    feat: 使用 CloudFlare 接入 Notion API

--- src/app.tsx
+++ src/app.tsx
@@ -1,15 +1,30 @@
-import React from "react";
 import { createRoot } from "react-dom/client";
+import { Client } from "@notionhq/client";
+// api key 需要自行申请
+import { PROXY_URL, NOTION_KEY, NOTION_DATABASE_WEEK_TODO_ID } from "../api";
+
+const getFetchUrl = (url) =>
+  PROXY_URL + url.substring(url.indexOf("/api.notion.com"));
+
+const notion = new Client({
+  auth: NOTION_KEY,
+  fetch: async (url, init) => fetch(getFetchUrl(url), init),
+});
+
+async function getDataBase() {
+  const res = await notion.databases.query({
+    database_id: NOTION_DATABASE_WEEK_TODO_ID,
+  });
+}
 
 const App = () => {
   return <>Hello Craft Extension</>;
 };
 
 const initApp = () => {
+  getDataBase();
   const container = document.getElementById("grace");
-  console.log("container", container);
   const root = createRoot(container);
-  console.log("root", root);
   root.render(<App />);
 };
 
